3eacc255677f7adc7230dadf1f03dfb9d05517f1
kyleodotcodotuk <kyle1359@gmail.com>
2021-04-23 23:08:02 +0100

    404

--- src/Footer.js
+++ src/Footer.js
@@ -6,12 +6,12 @@ function Footer() {
 
         <p>London, UK <img src="./svg/contact-ic.svg" alt="location" /><br />
         Kyle O'Connor &middot; https://kyleo.co.uk &middot; <a href="/legal" alt="legal">Legal Notice</a><br />
-        2021 &middot; My own photographs &middot; Coded with <a target="_blank" href="https://github.com/kyleodotcodotuk" rel="noreferrer">React</a></p>
+        2021 &middot; My own photographs &middot; Coded with <a target="_blank" href="https://github.com/kyleodotcodotuk/kyleocouk" rel="noreferrer">React</a></p>
 
         <ul class="social-icons">
-          <li><a href="https://twitter.com/kyleocouk" class="social-icon" alt=""> <i class="fa fa-twitter"></i></a></li>
-          <li><a href="mailto:kyle@kyleo.co.uk" class="social-icon" alt=""> <i class="fa fa-envelope"></i></a></li>
-          <li><a href="https://github.com/kyleodotcodotuk" class="social-icon" alt=""> <i class="fa fa-github"></i></a></li>
+          <li><a href="https://twitter.com/kyleocouk" target="_blank" class="social-icon" alt=""> <i class="fa fa-twitter"></i></a></li>
+          <li><a href="mailto:kyle@kyleo.co.uk" target="_blank" class="social-icon" alt=""> <i class="fa fa-envelope"></i></a></li>
+          <li><a href="https://github.com/kyleodotcodotuk" target="_blank" class="social-icon" alt=""> <i class="fa fa-github"></i></a></li>
         </ul>
 
       </div>
