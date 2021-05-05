28a15b3eb94414ed4dadc74a55b7224c7f6106a6
kyleodotcodotuk <kyle1359@gmail.com>
2021-05-05 22:06:52 +0100

    Welcome change

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,14 +3,12 @@ function Welcome() {
   return (
     <section id="welcome">
       <div className="wrapper">
-        <h2>Welcome title</h2>
-
-        <p>Hello world, I'm Kyle</p>
+        <h2>Hello world, I'm Kyle</h2>
 
         <p>I am primarily a front end developer from Yorkshire now living in London
         </p>
 
-        {/* <p>I'm partially available for side projects subject to permission from conflict of interest of current employment</p> */}
+        <p>Currently employed, mind the grey, this site is currently serving testing purposes</p>
 
         <img src="/logo192.png" alt="welcome" id="welcome-image" />
 
