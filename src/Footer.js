f9e85bcc26fb90434ccf21b90faf664f54ce2997
kyleodotcodotuk <kyle1359@gmail.com>
2021-05-06 22:12:17 +0100

    Footer icons

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
       <div className="wrapper">
 
         <p>London, UK &nbsp;<i class="fa fa-map-pin"></i><br />
-        Kyle O'Connor &nbsp;<i class="fa fa-mouse-pointer"></i>&nbsp; https://kyleo.co.uk &nbsp;<i class="fa fa-legal"></i>&nbsp; <a href="https://raw.githubusercontent.com/kyleodotcodotuk/kyleocouk/main/LICENSE" alt="legal">Legal Notice</a><br />
-        2021 &nbsp;<i class="fa fa-calendar"></i>&nbsp; My own photographs &nbsp;<i class="fa fa-camera"></i> &nbsp;Coded with <a target="_blank" href="https://github.com/kyleodotcodotuk/kyleocouk" rel="noreferrer">React</a></p>
+        Kyle O'Connor &nbsp;<i class="fa fa-bicycle"></i>&nbsp; https://kyleo.co.uk &nbsp;<i class="fa fa-mouse-pointer"></i>&nbsp; <a href="https://raw.githubusercontent.com/kyleodotcodotuk/kyleocouk/main/LICENSE" alt="legal">Legal Notice</a>  &nbsp;<i class="fa fa-legal"></i>&nbsp;<br />
+        2021 &nbsp;<i class="fa fa-calendar"></i>&nbsp; My own photographs &nbsp;<i class="fa fa-camera"></i> &nbsp;Coded with <a target="_blank" href="https://github.com/kyleodotcodotuk/kyleocouk" rel="noreferrer">React</a> <i class="fa fa-code"></i>&nbsp; </p>
 
         <ul class="social-icons">
           <li><a href="https://twitter.com/kyleocouk" rel="noreferrer" target="_blank" class="social-icon" alt="Twitter"> <i class="fa fa-twitter"></i></a></li>
