47f921841abd87a1ede4b2dac15a6d99f3d6d10b
kyleodotcodotuk <kyle1359@gmail.com>
2021-05-15 00:03:46 +0100

    Reduced images

--- src/index.js
+++ src/index.js
@@ -7,14 +7,10 @@ import Skills from './Skills';
 import Footer from './Footer';
 import BackgroundSlider from 'react-background-slider';
 import image1 from './img/bgs/010.JPG';
+import image2 from './img/bgs/020.JPG';
 import image3 from './img/bgs/030.JPG';
 import image4 from './img/bgs/040.JPG';
 import image5 from './img/bgs/050.JPG';
-import image6 from './img/bgs/060.JPG';
-import image7 from './img/bgs/070.JPG';
-import image8 from './img/bgs/080.JPG';
-import image9 from './img/bgs/090.JPG';
-import image10 from './img/bgs/100.JPG';
 import reportWebVitals from './reportWebVitals';
 
 
@@ -22,8 +18,8 @@ ReactDOM.render(
 
   <React.StrictMode>
     <BackgroundSlider
-      images={[image1, image3, image4, image5, image6, image7, image8, image9, image10]}
-      duration={6} transition={2} shuffle />
+      images={[image1, image2, image3, image4, image5]}
+      duration={6} transition={2} />
     <Header />
     <Welcome />
     <Skills />
