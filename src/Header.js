9315a2fffcdede3ee9b64299c052b020f2ed82e2
kyleodotcodotuk <kyle1359@gmail.com>
2021-05-28 21:03:31 +0100

    GA

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,20 @@
+import ReactGA from 'react-ga';
+// import auth from './auth.ts'; // Sample authentication provider
+
+const trackingId = "UA-72070431-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+ReactGA.set({
+    // userId: auth.currentUserId(),
+    // any data that is relevant to the user session
+    // that you would like to track with google analytics
+})
 
 function Header() {
     return (
         <header>
 
-            <img src="./logo.png" alt="logo" id="logo" title="title" width="186" height="66" />
-            <img src="./mobile-logo.png" alt="logo" width="90" height="107" id="mobile-logo" title="title" />
+            <img src="./logo.png" alt="logo" id="logo" title="logo" width="186" height="66" />
+            <img src="./mobile-logo.png" alt="logo" width="90" height="107" id="mobile-logo" title="logo" />
 
             <h1>Kyle O&apos;Connor</h1>
 
