992638a1257c833b65987174f5ec107e9e4b3298
kyleodotcodotuk <kyle1359@gmail.com>
2021-05-28 21:08:00 +0100

    GA

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,4 @@
-import ReactGA from 'react-ga';
-// import auth from './auth.ts'; // Sample authentication provider
-
-const trackingId = "UA-72070431-1"; // Replace with your Google Analytics tracking ID
-ReactGA.initialize(trackingId);
-ReactGA.set({
-    // userId: auth.currentUserId(),
-    // any data that is relevant to the user session
-    // that you would like to track with google analytics
-})
-
-function Header() {
+export default function Header() {
     return (
         <header>
 
@@ -25,9 +14,6 @@ function Header() {
         </header>
 
 
-
     );
 }
-
-export default Header;
 
