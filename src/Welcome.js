6fb0de2a1e47e5e8d3d0bdd6579930910a62648f
kyleodotcodotuk <kyle1359@gmail.com>
2021-06-04 23:20:39 +0100

    Welcome

--- src/Welcome.js
+++ src/Welcome.js
@@ -15,9 +15,6 @@ function Welcome() {
           I used to primarily create websites for schools but now I'm working on websites for an array of businesses.
         </p>
 
-        <p>Over the years I have worked on maintaining wireframes, created templates, fixed bugs, setup development environments and sourced/created code snippets.<br />
-        My duties also extend to communicating with other departments & directing the development department with training and meetings.</p>
-
         <p>Currently employed as a Web Developer and Designer in London, this site is currently serving testing purposes.</p>
 
         <img src={me} title="My Avatar" alt="" id="welcome-image" width="250" height="250" />
