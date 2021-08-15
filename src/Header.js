662157ab4a36e554f9ed28fb9baa9edd595225d1
kyleodotcodotuk <kyle1359@gmail.com>
2021-08-15 17:32:22 +0100

    Pattern

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,10 @@
+// import logo from './logo.svg';
+
 export default function Header() {
     return (
         <header>
 
+            {/* <img src={logo} alt="logo" id="logo" title="logo"  /> */}
             <img src="/logo.png" alt="logo" id="logo" title="logo" width="186" height="66" />
             <img src="/mobile-logo.png" alt="logo" width="90" height="107" id="mobile-logo" title="logo" />
 
