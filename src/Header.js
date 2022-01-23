7de629e6087028df9841e642d6cca858d0351b7a
kyleodotcodotuk <kyle1359@gmail.com>
2022-01-23 14:29:16 +0000

    Tweaks

--- src/Header.js
+++ src/Header.js
@@ -8,7 +8,7 @@ export default function Header() {
 
                 <img src={tempLogo} alt="Logo" id="logo" title="Logo" />
                 {/* <img src="/logo.png" alt="logo" id="logo" title="logo" width="186" height="66" /> */}
-                <img src={tempLogo} alt="Logo" width="90" height="107" id="mobile-logo" title="logo" />
+                <img src={tempLogo} alt="Logo" width="200" height="220" id="mobile-logo" title="logo" />
 
                 <h1>Kyle O&apos;Connor<br />
 
