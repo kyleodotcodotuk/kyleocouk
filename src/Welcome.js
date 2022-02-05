1f8a37269a2d54dd6c9b1ca84eb46b8d6fa8c8c6
kyleodotcodotuk <kyle1359@gmail.com>
2022-02-05 11:51:35 +0000

    404

--- src/Welcome.js
+++ src/Welcome.js
@@ -7,7 +7,7 @@ export default function Welcome() {
       <div className="wrapper">
         <h1>Hello world <i className="fa fa-home"></i></h1>
 
-        <p>I am website developer from Yorkshire now living in London, I moved there in the summer of 2016 to start my career as a developer.
+        <p>I am a professional website developer from Yorkshire now living in London, I moved there in the summer of 2016 to start my career as a developer.
         </p>
 
         <p>After graduating with my degree in Web Design from the University of Bradford, I have gained over 6 years of commercial experience. During which I have coded over 500 bespoke websites and templates with editable features.</p>
