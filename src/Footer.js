2a00bda018c9ff40c4a93318bcdc15a75954d144
kyleodotcodotuk <kyle1359@gmail.com>
2022-12-03 18:37:53 +0000

    Performance

--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Twitter from './icons/twitter.svg';
+import Email from './icons/email.svg';
+import GitHub from './icons/github.svg';
 
 export default function Footer() {
 
@@ -11,9 +14,9 @@ export default function Footer() {
           <a href="/docs/privacy_policy.pdf" target="_blank" alt="Privacy Policy" title="Privacy Policy">Privacy Policy</a>   </p>
 
         <ul className="social-icons">
-          <li><a href="https://twitter.com/kyleocouk" rel="noreferrer" target="_blank" className="social-icon" alt="Twitter" title="Twitter"> <i className="fa fa-twitter"></i></a></li>
-          <li><a href="mailto:info@kyleo.co.uk" target="_blank" rel="noreferrer" className="social-icon" alt="Email" title="Email"> <i className="fa fa-envelope"></i></a></li>
-          <li><a href="https://github.com/kyleodotcodotuk" target="_blank" rel="noreferrer" className="social-icon" alt="GitHub" title="GitHub"> <i className="fa fa-github"></i></a></li>
+          <li><a href="https://twitter.com/kyleocouk" rel="noreferrer" target="_blank" className="social-icon" alt="Twitter" title="Twitter"> <img src={Twitter} className="fa fa-twitter" alt="Twitter" title="Twitter" /> </a></li>
+          <li><a href="mailto:info@kyleo.co.uk" target="_blank" rel="noreferrer" className="social-icon" alt="Email" title="Email"> <img src={Email} className="fa fa-envelope" alt="Email" title="Email" /></a></li>
+          <li><a href="https://github.com/kyleodotcodotuk" target="_blank" rel="noreferrer" className="social-icon" alt="GitHub" title="GitHub"> <img src={GitHub} className="fa fa-github" alt="GitHub" title="GitHub" /></a></li>
         </ul>
 
       </div>
