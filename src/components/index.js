b39904bf032b80c20f350f49469fb06a5da1d907
kyleodotcodotuk <kyle1359@gmail.com>
2023-12-01 14:26:40 +0000

    Delete

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Navigation } from "./Navigation";
-export { default as Overview } from "./Overview";
-export { default as Blogfeed } from "./Blogfeed";
-// export { default as Portfolio } from "./Portfolio"; 
