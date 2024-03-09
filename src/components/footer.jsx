17c750dfa62f1bc02e52e94f8d0750a5a3604948
kyleodotcodotuk <kyle1359@gmail.com>
2024-03-09 22:39:11 +0000

    Minimal footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,10 +5,9 @@ export default function Footer() {
     <footer>
       <div className="wrapper">
 
-        <p>{new Date().getFullYear()} &middot; Kyle O'Connor<br />
-          Tameside, Greater Manchester<br />
-          United Kingdom
+        <p>{new Date().getFullYear()} &middot; <a href="/docs/privacy_policy.pdf" target="_blank" rel="noreferrer">Privacy Policy</a>
         </p>
+        
         <ul className="social-icons">
           <li><a href="https://github.com/kyleodotcodotuk" target="_blank" rel="noreferrer" className="social-icon" alt="GitHub" title="GitHub"> <img src={GitHub} className="fa fa-github" alt="GitHub" title="GitHub" /></a></li>
         </ul>
