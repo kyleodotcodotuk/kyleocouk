a8d7e43594107551d996a0f538ab6a131b6a76df
kyleodotcodotuk <kyle1359@gmail.com>
2024-03-21 20:32:57 +0000

    Time updates

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,21 @@
-import Me from "../img/me.png";
+import React, { useState, useEffect } from 'react';
+import Me from "../img/me.png"; 
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
 
 export default function Header() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <header>
       <div className="centered">
@@ -18,6 +33,14 @@ export default function Header() {
           <h1>Kyle O'Connor</h1>
           <br />
           <h2>UI Developer</h2>
+        </div>
+
+        <div className="date-and-time">
+          <p>
+            <strong>{currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}</strong>
+            <span>|</span>
+            {`${monthNames[currentTime.getMonth()]} ${currentTime.getDate()}, ${currentTime.getFullYear()}`}
+          </p>
         </div>
       </div>
     </header>
