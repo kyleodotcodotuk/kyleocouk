c4c6794a66bf90ec5e1ba359e9391f701ad3decb
kyleodotcodotuk <kyle1359@gmail.com>
2024-03-28 22:56:13 +0000

    Footer UI

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,19 +1,36 @@
-import GitHub from './../icons/github.svg';
-import CodePen from './../icons/codepen.svg';
+import GitHub from "./../icons/github.svg";
 
 export default function Footer() {
   return (
     <footer>
       <div className="wrapper">
-
-        <p>{new Date().getFullYear()} &middot; <a href="/docs/privacy_policy.pdf" target="_blank" rel="noreferrer">Privacy Policy</a>
+        <p>
+          {new Date().getFullYear()} &middot;{" "}
+          <a href="/docs/privacy_policy.pdf" target="_blank" rel="noreferrer">
+            Privacy Policy
+          </a>
         </p>
-        
-        <ul className="social-icons">
-          <li><a href="https://github.com/kyleodotcodotuk" target="_blank" rel="noreferrer" className="social-icon" alt="GitHub" title="GitHub"> <img src={GitHub} className="fa fa-github" alt="GitHub" title="GitHub" /></a></li>
-          <li><a href="https://codepen.io/kyleodotcodotuk" target="_blank" rel="noreferrer" className="social-icon" alt="Codepen" title="Codepen"> <img src={CodePen} className="fa fa-codepen" alt="Codepen" title="Codepen" /></a></li>
-        </ul>
 
+        <ul className="social-icons">
+          <li>
+            <a
+              href="https://github.com/kyleodotcodotuk"
+              target="_blank"
+              rel="noreferrer"
+              className="social-icon"
+              alt="GitHub"
+              title="GitHub"
+            >
+              {" "}
+              <img
+                src={GitHub}
+                className="fa fa-github"
+                alt="GitHub"
+                title="GitHub"
+              />
+            </a>
+          </li>
+        </ul>
       </div>
     </footer>
   );
