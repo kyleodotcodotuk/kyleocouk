0a9e7e28eed8b6fd17b375cf8d347a1a8537667a
kyleodotcodotuk <kyle1359@gmail.com>
2024-09-21 16:52:15 +0100

    Removing assets to start over

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Header } from "./header";
-export { default as Posts } from "./posts";
 export { default as Timeline } from "./timeline";
 export { default as Footer } from "./footer";
